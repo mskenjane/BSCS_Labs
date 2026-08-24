7805a66a8fcebbef007311b40618d15266abdebc
MsKenjane <MsKenjane1705@gmail.com>
2026-08-24 10:22:47 +0800

    pushing Lab 5 folder

---
+++ Lab 5/TableWeb.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Table Test</title>
+</head>
+<body>
+    <div class="Container">
+        <table border="1">
+            <tr>
+                <td >Akong Web</td>
+                <td>Logo</td>
+                <td>About Us</td>
+                <td>Produkto</td> 
+                <td>Tawgan kami</td>
+            </tr>
+
+            <tr>
+                <td colspan = "5">
+                    <marquee behavior="left" direction="">Breaking News: Inde ko kabalo mag code</marquee>
+                </td>
+            </tr>
+
+            <div class="MyList">
+                <tr>
+                    <td colspan = "1">
+                        <ul>
+                            <li>Food</li>
+                            <li>Pagkaon</li>
+                            <li>Comida</li>
+                            <li>Ambot</li>
+                        </ul>
+                    </td>
+                    <td colspan = "9">Content 2</td>
+                </tr>
+            </div>
+
+            <tr>
+                <td colspan = "10"> Ambot ano ni unod </td>
+            </tr>
+
+            <tr>
+                <td colspan = "10"> Footer </td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
